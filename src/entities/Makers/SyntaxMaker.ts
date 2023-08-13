643ad1613f520545e907d35905529fc13dedfe22
drezzyts <drezzydev@gmail.com>
2023-08-13 19:59:44 -0300

    feat(backend): creating interpreter and runtime

--- src/entities/Makers/SyntaxMaker.ts
+++ src/entities/Makers/SyntaxMaker.ts
@@ -1,6 +1,49 @@
-import { SyntaxKind, SyntaxToken, SyntaxTokenPosition } from "../../types/syntax";
+import { 
+  BinarySyntaxExpression, 
+  NumberSyntaxExpression, 
+  ParenthesizedSyntaxExpression, 
+  SyntaxExpression, 
+  SyntaxExpressionType, 
+  SyntaxKind, 
+  SyntaxToken, 
+  SyntaxTokenPosition, 
+  UnarySyntaxExpression 
+} from "../../types/syntax";
 
 export default class SyntaxMaker {
+  public static mkUnaryExpressionSyntax(
+    operator: SyntaxToken, 
+    operand: SyntaxExpressionType
+  ): UnarySyntaxExpression {
+    return {
+      kind: SyntaxKind.UnaryExpression,
+      operand,
+      operator
+    } as UnarySyntaxExpression;
+  }
+  
+  public static mkBinaryExpressionSyntax(
+    left: SyntaxExpression, 
+    operator: SyntaxToken, 
+    right: SyntaxExpression
+  ): BinarySyntaxExpression {
+    return { 
+      kind: SyntaxKind.BinaryExpression, 
+      left, 
+      operator, 
+      right 
+    } as BinarySyntaxExpression;
+  }
+
+  public static makeParenthesizedExpression(left: SyntaxToken, expression: SyntaxExpression, right: SyntaxToken): ParenthesizedSyntaxExpression {
+    return { 
+      kind: SyntaxKind.ParenthesizedExpression, 
+      left, 
+      expression, 
+      right 
+    } as ParenthesizedSyntaxExpression;
+  }
+
   public static makeToken(
     kind: SyntaxKind, 
     position: SyntaxTokenPosition, 
@@ -8,5 +51,12 @@ export default class SyntaxMaker {
     value: number | null
   ): SyntaxToken {
     return { kind, position, text, value } as SyntaxToken;
+  }
+
+  public static makeNumberExpression(
+    token: SyntaxToken,
+    value: number
+  ): NumberSyntaxExpression {
+    return { kind: SyntaxKind.NumberExpression, token, value } as NumberSyntaxExpression;
   }
 }
--- src/types/syntax.ts
+++ src/types/syntax.ts
@@ -3,15 +3,19 @@ export enum SyntaxKind {
   EOF,
   BadToken,
   NumberToken,
+  CaretToken,
+  StarToken,
+  SlashToken,
   PlusToken,
   MinusToken,
-  SlashToken,
-  StarToken,
+  OpenParenthesisToken,
+  CloseParenthesisToken,
 
   // Expressions
   BinaryExpression,
   UnaryExpression,
-  ParenthesizedExpression
+  ParenthesizedExpression,
+  NumberExpression,
 }
 
 export interface SyntaxToken {
@@ -26,3 +30,35 @@ export type SyntaxTokenPosition = [number, number];
 export interface SyntaxExpression {
   kind: SyntaxKind
 }
+
+export interface NumberSyntaxExpression extends SyntaxExpression {
+  kind: SyntaxKind.NumberExpression,
+  token: SyntaxToken,
+  value: number
+}
+
+
+export interface BinarySyntaxExpression extends SyntaxExpression {
+  kind: SyntaxKind.BinaryExpression;
+  left: SyntaxExpression;
+  operator: SyntaxToken;
+  right: SyntaxExpression;
+}
+
+export interface UnarySyntaxExpression extends SyntaxExpression {
+  kind: SyntaxKind.UnaryExpression;
+  operator: SyntaxToken;
+  operand: SyntaxExpression;
+}
+
+export interface ParenthesizedSyntaxExpression extends SyntaxExpression {
+  kind: SyntaxKind.ParenthesizedExpression;
+  left: SyntaxToken;
+  expression: SyntaxExpression;
+  right: SyntaxToken;
+}
+
+export type SyntaxExpressionType = BinarySyntaxExpression |
+  UnarySyntaxExpression | 
+  ParenthesizedSyntaxExpression |
+  NumberSyntaxExpression;
