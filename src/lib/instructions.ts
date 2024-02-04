29089bf7bb7bfab3be4c8537535fb2a66ea92da1
drezzyts <drezzydev@gmail.com>
2024-02-03 21:57:41 -0300

    fix(instructions): fixing instructions constants parameters

--- src/lib/instructions.ts
+++ src/lib/instructions.ts
@@ -1,6 +1,8 @@
 import Enviroment from "../structs/Enviroment";
 
 export function constant(_enviroment: Enviroment, key: string, value: number) {
+  if (typeof value !== 'number') value = _enviroment.constants.get(value as string) || 0;
+  
   _enviroment.constants.set(key, value);
 }
 
